e0c8e7da1abaaf97b952f65d7aa7e9e3602ca973
russell13192 <murrayge@mnstate.edu>
2019-06-29 15:43:53 -0700

    LaneFinder declaration

--- development/perception/lane_following/include/LaneFinder.h
+++ development/perception/lane_following/include/LaneFinder.h
@@ -5,11 +5,20 @@
 #ifndef AUTONOMOUS_DRIVING_STACK_LANEFINDER_H
 #define AUTONOMOUS_DRIVING_STACK_LANEFINDER_H
 
+#include <opencv/opencv.hpp>
 #include "opencv2/highgui.hpp"
+
+using namespace cv;
 
 class LaneFinder {
 private:
-
+	VideoCapture cap_;
+public:
+	string file_path_;
+	LaneFinder(string file_path_) : file_path_(file_path_)
+	{
+		cap_(file_path_);
+	}
 };
 
 #endif //AUTONOMOUS_DRIVING_STACK_LANEFINDER_H
--- development/perception/lane_following/src/main.cpp
+++ development/perception/lane_following/src/main.cpp
@@ -2,11 +2,14 @@
  *
  */
 
+#include <opencv/opencv.hpp>
 #include <iostream>
-#include "opencv2/highgui.hpp"
 
-int main()
+
+int main(int argc, char* argv)
 {
     // TODO: Instantiate LaneFinder object
+    VideoCapture cap("/home/russell/Autonomous-Driving-Stack/development/perception/lane_following/test_videos/lane_test_1.mp4");
+    std::cout << "Success" << endl;
     return 0;
 }
